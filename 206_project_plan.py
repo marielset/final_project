7cd463608698feb698b1a573eb1c7afbddd7731d
msetton <msetton@umich.edu>
2017-04-09 21:55:07 -0400

    added files

---
+++ 206_project_plan.py
@@ -0,0 +1,34 @@
+## Your name: Mariel Setton
+## The option you've chosen: project 2!
+
+# Put import statements you expect to need here!
+
+
+import unittest
+import itertools
+import collections
+import tweepy
+import twitter_info # same deal as always...
+import json
+import sqlite3
+import random
+import re 
+from bs4 import BeautifulSoup
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your test cases here.
+
+
+## Remember to invoke all your tests...
